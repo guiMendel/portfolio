7d619bea8d30f44cb908069ba3f437556d30f11c
guimendel <guimendeln@gmail.com>
2023-04-17 19:47:03 -0300

    🔨Tie tabs to displayed content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/App/app.component';
+import { AppComponent } from './components/app/app.component';
+import { PortfolioViewComponent } from './components/portfolio-view/portfolio-view.component';
+import { ExperienceViewComponent } from './components/experience-view/experience-view.component';
+import { AboutViewComponent } from './components/about-view/about-view.component';
+import { ContactViewComponent } from './components/contact-view/contact-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioViewComponent,
+    ExperienceViewComponent,
+    AboutViewComponent,
+    ContactViewComponent
   ],
   imports: [
     BrowserModule,
