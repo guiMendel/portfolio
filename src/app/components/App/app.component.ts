64c61f816adfb50efa4aa27d49122cdd8165d35e
guimendel <guimendeln@gmail.com>
2023-04-17 18:59:10 -0300

    🔨Switch tabs on click

--- src/app/components/App/app.component.ts
+++ src/app/components/App/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  // Which tab is currently selected
+  selectedTabIndex = 0;
+
+  trackTab(_: number, tab: AppComponent['tabs'][number]) {
+    return tab.title;
+  }
+
+  tabs = [
+    { title: 'Portfolio' },
+    { title: 'Experience' },
+    { title: 'About' },
+    { title: 'Contact' },
+  ];
 }
