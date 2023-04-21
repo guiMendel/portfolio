2b27ea907cf8afad2e76d4f0bd1f8f99821771fb
guimendel <guimendeln@gmail.com>
2023-04-20 22:11:33 -0300

    🔨Persist toggle

--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -1,16 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.component.html',
   styleUrls: ['./toggle.component.scss'],
 })
-export class ToggleComponent {
+export class ToggleComponent implements OnInit {
   @Input() value = false
   @Output() valueChange = new EventEmitter<boolean>()
 
-  toggleValue() {
-    this.value = !this.value
+  @Input() storageKey?: string
+
+  setValue(value: boolean) {
+    this.value = value
     this.valueChange.emit(this.value)
+
+    if (this.storageKey != undefined)
+      localStorage.setItem(this.storageKey, JSON.stringify(this.value))
+  }
+
+  toggleValue() {
+    this.setValue(!this.value)
+  }
+
+  ngOnInit(): void {
+    if (
+      this.storageKey != undefined &&
+      localStorage.getItem(this.storageKey) != null
+    )
+      this.setValue(JSON.parse(localStorage.getItem(this.storageKey)!))
   }
 }
